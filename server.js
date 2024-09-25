70bc7f9139249df7b1e8a5d8e2033f7224e1a61b
VedantDongare <dongrevedant79@gmail.com>
2024-09-25 18:43:50 +0530

    login and register page working now

--- server.js
+++ server.js
@@ -17,7 +17,7 @@
     });
 
     app.get('/register', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public'));
+      res.sendFile(path.join(__dirname, 'public','register.html'));
     });
 
 
